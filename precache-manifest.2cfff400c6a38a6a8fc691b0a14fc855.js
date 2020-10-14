f38526c033884a0712eb76c6c3d424970d1737e4
George Lioris <george.lioris@gmail.com>
2020-10-14 21:14:29 +0300

    Updates

---
+++ precache-manifest.2cfff400c6a38a6a8fc691b0a14fc855.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc071c61135b5cd25173de8bf29f23be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87152bac6ef44e2c734f",
+    "url": "/static/css/main.b5156aee.chunk.css"
+  },
+  {
+    "revision": "f916e5ace4d1dc63c3e0",
+    "url": "/static/js/2.a20ac5a7.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/static/js/2.a20ac5a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87152bac6ef44e2c734f",
+    "url": "/static/js/main.9fce3558.chunk.js"
+  },
+  {
+    "revision": "fc31250b46921b763fda",
+    "url": "/static/js/runtime-main.a0be316b.js"
+  },
+  {
+    "revision": "7b60d393ba48f20d082a4305d002f021",
+    "url": "/static/media/aerity768w.7b60d393.png"
+  },
+  {
+    "revision": "50b31d014f39c33827bcb7f6e9cfcfef",
+    "url": "/static/media/aerity768w2.50b31d01.png"
+  },
+  {
+    "revision": "776e744f29d65986cf765777f202a172",
+    "url": "/static/media/algovis768w.776e744f.png"
+  },
+  {
+    "revision": "49e4dd72ce602941a924d9fed700c836",
+    "url": "/static/media/algovis768w2.49e4dd72.png"
+  },
+  {
+    "revision": "a8cd1579b428b6765555132b4f4d78fa",
+    "url": "/static/media/avatar.a8cd1579.jpg"
+  },
+  {
+    "revision": "dac39ff64a1853c3f3de4bf594424475",
+    "url": "/static/media/zstore768w.dac39ff6.png"
+  },
+  {
+    "revision": "dc5fcc0a02e606a85639d56c61010502",
+    "url": "/static/media/zstore768w2.dc5fcc0a.png"
+  }
+]);
